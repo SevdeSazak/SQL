d210cf6c19264d9a532922502cbdbed2de048d8c
Sevde Sazak <sazaksevde@gmail.com>
2022-05-08 11:28:13 +0300

    AVG - MAX - MIN

---
+++ 6.sql
@@ -0,0 +1,12 @@
+--Sql Sorgulari 6
+--film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+SELECT AVG(rental_rate) FROM film;
+
+--film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+SELECT COUNT(*) FROM film WHERE film LIKE 'C%';
+
+--film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+SELECT MAX(lenght) FROM film WHERE rental_rate = 0.99;
+
+--film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+SELECT COUNT(DISTRICT replacement_cost) FROM film WHERE lenght> 150;
